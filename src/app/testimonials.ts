b73a3ae1079cb6b989e0c01fd58e9f465282f5c2
Shaeen09 <shaheenshaikh09@gmail.com>
2023-02-28 11:27:04 +0100

    something

--- src/app/testimonials.ts
+++ src/app/testimonials.ts
@@ -1,48 +1,49 @@
 import { HtmlParser } from "@angular/compiler";
 
 export let testimonials=[
+    // {
+    //     fname:"John",
+    //     lname:"Sender",
+    //     date:"1.01.2020",
+    //     company:"HR Professional,Actelion Pharmaceuticals",
+    //     comment:"Ed took some great photos at our recent conference in September. He was very patient and adaptable and mixed in well with all of us. The photos included work and social activities and captured the spirit perfectly.",
+    //     rating:"&#9734;&#9734;&#9734;&#9734;&#9734;"
+    // },{
+    //     fname:"Anna",
+    //     lname:"Colley",
+    //     date:"13.02.2021",
+    //     company:"Director,Ayre Media",
+    //     comment:" I can highly recommend Vicker for all your company's need in photography, we hired him for 3 events and his photos are outstanding, very personal and eye fetching. We surly will contract him again!",
+    //     rating:"&#9734;&#9734;&#9734;&#9734;&#9734;"
+    // },{
+    //     fname:"Helen",
+    //     lname:"Gardener",
+    //     date:"25.05.2021",
+    //     company:"EduAcademy",
+    //     comment:"I haveve worked with the company in several roles at different organisations, simply because I am yet to find a better photographer anywhere! Vicker really gets the brief every time, is professional, reliable, flexible and generally lovely to work with. He has given us some brilliant shots we use again and again,highly recommend",
+    //     rating:"&#9734;&#9734;&#9734;&#9734;&#9734;"
+    // },{
+    //     fname:"Amy",
+    //     lname:"Downing",
+    //     date:"30.09.2022",
+    //     company:"Marie Curie Cancer Care",
+    //     comment:"Thank you very much for the photos of the Brain Game, they are brilliant and just what we wanted. I am really pleased with how the new light design came across in the photos and you have captured the evenings atmosphere wonderfully!",
+    //     rating:"&#9734;&#9734;&#9734;&#9734;&#9734;"
+    // },{
+    //     fname:"Markus",
+    //     lname:"Weatheritt ",
+    //     date:"11.01.2022",
+    //     company:"Wagner and Son",
+    //     comment:"We use Vickers across several of our brands. He provides us with a professional service every time and his work helps us to market future events. His images are high quality and present and promote our brand well",
+    //     rating:"&#9734;&#9734;&#9734;&#9734;&#9734;"
+    // },
     {
-        fname:"John",
-        lname:"Sender",
-        date:"1.01.2020",
-        company:"HR Professional,Actelion Pharmaceuticals",
-        comment:"Ed took some great photos at our recent conference in September. He was very patient and adaptable and mixed in well with all of us. The photos included work and social activities and captured the spirit perfectly.",
-        rating:"&#9734;&#9734;&#9734;&#9734;&#9734;"
-    },{
-        fname:"Anna",
-        lname:"Colley",
-        date:"13.02.2021",
-        company:"Director,Ayre Media",
-        comment:" I can highly recommend Vicker for all your company's need in photography, we hired him for 3 events and his photos are outstanding, very personal and eye fetching. We surly will contract him again!",
-        rating:"&#9734;&#9734;&#9734;&#9734;&#9734;"
-    },{
-        fname:"Helen",
-        lname:"Gardener",
-        date:"25.05.2021",
-        company:"EduAcademy",
-        comment:"I haveve worked with the company in several roles at different organisations, simply because I am yet to find a better photographer anywhere! Vicker really gets the brief every time, is professional, reliable, flexible and generally lovely to work with. He has given us some brilliant shots we use again and again,highly recommend",
-        rating:"&#9734;&#9734;&#9734;&#9734;&#9734;"
-    },{
-        fname:"Amy",
-        lname:"Downing",
-        date:"30.09.2022",
-        company:"Marie Curie Cancer Care",
-        comment:"Thank you very much for the photos of the Brain Game, they are brilliant and just what we wanted. I am really pleased with how the new light design came across in the photos and you have captured the evenings atmosphere wonderfully!",
-        rating:"&#9734;&#9734;&#9734;&#9734;&#9734;"
-    },{
-        fname:"Markus",
-        lname:"Weatheritt ",
-        date:"11.01.2022",
-        company:"Wagner and Son",
-        comment:"We use Vickers across several of our brands. He provides us with a professional service every time and his work helps us to market future events. His images are high quality and present and promote our brand well",
-        rating:"&#9734;&#9734;&#9734;&#9734;&#9734;"
-    },{
         fname:"Chloe",
         lname:"Bass",
         date:"19.01.2023",
         company:"FlightGlobal",
         comment:"vicker is a professional and regular photographer for our corporate events, and always delivers a really high selection of photos. He's extremely responsive and easy to work with pre and post event too. I would always use him where possible",
-        rating: &#11088;
+        rating: "<span>&#11088;</span>"
     }
  ];
 
@@ -52,5 +53,5 @@ export let testimonials=[
   date: string;
   company:string;
   comment:string;
-  rating:any;
+  rating: string;
  }
